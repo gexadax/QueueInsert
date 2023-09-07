e5dc0a1cde7885039681391b22d3cc11720eed15
Andrey Kurbanov <kurban1111@mail.ru>
2023-09-07 06:26:24 +0300

    Adding comments

--- QueueInsert.cpp
+++ QueueInsert.cpp
@@ -2,11 +2,13 @@
 
 FineGrainedQueue::FineGrainedQueue()
 {
+    // Initialize the head of the queue as nullptr.
     head = nullptr;
 }
 
 FineGrainedQueue::~FineGrainedQueue()
 {
+    // Destructor to clean up allocated memory for nodes.
     while (head != nullptr)
     {
         Node* temp = head;
@@ -17,17 +19,20 @@ FineGrainedQueue::~FineGrainedQueue()
 
 void FineGrainedQueue::insertIntoMiddle(int value, int pos)
 {
+    // Create a new node with the given value.
     Node* new_node = new Node;
     new_node->value = value;
     new_node->next = nullptr;
     new_node->node_mutex = std::make_unique<std::mutex>();
 
+    // Acquire a lock on the queue's mutex to protect the concurrent access to the queue.
     std::unique_lock<std::mutex> queue_lock(queue_mutex);
 
     Node* current = head;
     Node* previous = nullptr;
     int current_pos = 0;
 
+    // Traverse the queue to find the insertion position.
     while (current_pos < pos && current != nullptr) {
         previous = current;
         current = current->next;
@@ -35,8 +40,10 @@ void FineGrainedQueue::insertIntoMiddle(int value, int pos)
     }
 
     if (previous != nullptr) {
+        // Acquire a lock on the previous node's mutex.
         std::unique_lock<std::mutex> current_lock(*(previous->node_mutex));
         if (current) {
+            // If there is a next node, acquire a lock on its mutex.
             std::unique_lock<std::mutex> next_lock(*(current->node_mutex));
             new_node->next = current;
             previous->next = new_node;
@@ -46,6 +53,7 @@ void FineGrainedQueue::insertIntoMiddle(int value, int pos)
         }
     }
     else {
+        // If there is no previous node, insert the new node at the beginning of the queue.
         new_node->next = head;
         head = new_node;
     }
--- main.cpp
+++ main.cpp
@@ -4,28 +4,35 @@
 
 int main()
 {
+    // Create a FineGrainedQueue instance.
     FineGrainedQueue queue;
 
+    // Insert values from 0 to 9 into the middle of the queue.
     for (int i = 0; i < 10; i++)
     {
         queue.insertIntoMiddle(i, i);
     }
 
+    // Create a vector of threads to simulate concurrent insertions.
     std::vector<std::thread> threads;
     threads.reserve(7);
 
+    // Spawn seven threads to insert values into the middle of the queue concurrently.
     for (int i = 0; i < 7; i++) {
         threads.emplace_back([&queue, i]() {
             queue.insertIntoMiddle(100 * (i + 1), 5 + i);
             });
     }
 
+    // Join all the threads to wait for them to complete.
     for (auto& thread : threads) {
         thread.join();
     }
 
+    // Retrieve the head of the queue.
     Node* current = queue.getHead();
 
+    // Print the values in the queue.
     while (current != nullptr)
     {
         std::cout << current->value << " ";
