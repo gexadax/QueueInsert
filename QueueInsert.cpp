e70f97ae3ad4dd98599b106d3c41cf20b649d141
Andrey Kurbanov <kurban1111@mail.ru>
2023-09-07 06:01:59 +0300

    Some lock and mutex fixes

--- QueueInsert.cpp
+++ QueueInsert.cpp
@@ -19,8 +19,8 @@ void FineGrainedQueue::insertIntoMiddle(int value, int pos)
 {
     Node* new_node = new Node;
     new_node->value = value;
-    new_node->node_mutex = std::make_unique<std::mutex>();
     new_node->next = nullptr;
+    new_node->node_mutex = std::make_unique<std::mutex>();
 
     {
         std::lock_guard<std::mutex> queue_lock(queue_mutex);
@@ -29,21 +29,21 @@ void FineGrainedQueue::insertIntoMiddle(int value, int pos)
         Node* previous = nullptr;
         int current_pos = 0;
 
-        while (current_pos < pos - 1 && current != nullptr && current->next != nullptr) {
+        while (current_pos < pos - 1 && current != nullptr) {
             previous = current;
             current = current->next;
             current_pos++;
         }
 
-        if (current != nullptr) {
-            std::unique_lock<std::mutex> current_lock(*(current->node_mutex));
-            std::mutex* next_mutex = (current->next != nullptr) ? current->next->node_mutex.get() : new std::mutex;
-            std::unique_lock<std::mutex> next_lock(*next_mutex);
+        if (previous != nullptr) {
+            std::unique_lock<std::mutex> current_lock(*(previous->node_mutex));
+            std::unique_lock<std::mutex> next_lock(*(new_node->node_mutex));
 
-            new_node->next = current->next;
-            current->next = new_node;
+            new_node->next = previous->next;
+            previous->next = new_node;
         }
         else {
+            std::unique_lock<std::mutex> next_lock(*(new_node->node_mutex));
             new_node->next = head;
             head = new_node;
         }
